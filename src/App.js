bab6627c0ad89279259b0d2d5d8837eb1838f433
3imed-jaberi <imed_jebari@hotmail.fr>
2019-11-15 22:06:49 +0100

    cleanup & styling with bootstrap / css 💅🏻 ..

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/img/react.png';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main"> { /* => using basic css class */ }
+      <h1 className="alert alert-success" style={{ background: "green", color: "white",   marginTop: '100px' }}> Welcome to <img src={logo} /> ! </h1> {/* => using bootstrap class */}
+      <br />
+      {/* content */}
+      
+
     </div>
   );
 }
+
 
 export default App;
